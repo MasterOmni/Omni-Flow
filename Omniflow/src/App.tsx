41c6b1845eea88ef60f278cdf3387c6324430b73
Omni <32719461+MasterOmni@users.noreply.github.com>
2026-03-01 21:08:26 +0000

    Add files via upload

---
+++ Omniflow/src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Omni-flow</h1>
+      <p>Your deployment is working 🎉</p>
+    </div>
+  );
+}
