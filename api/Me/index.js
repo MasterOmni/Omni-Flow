ab9c2b88147b32ef1e58e58e2cb7a808d9cf5622
Omni <32719461+MasterOmni@users.noreply.github.com>
2026-03-02 21:47:17 +0000

    Add files via upload

---
+++ api/Me/index.js
@@ -0,0 +1,15 @@
+module.exports = async function (context, req) {
+  const userId =
+    req.headers["x-ms-client-principal-id"] ||
+    req.headers["x-ms-client-principal-name"] ||
+    "local-dev-user";
+
+  const name =
+    req.headers["x-ms-client-principal-name"] ||
+    "Local Dev";
+
+  context.res = {
+    status: 200,
+    jsonBody: { userId, name, role: "operative" }
+  };
+};
